9eadaf50ff17848a19cc477480dadbd697d5d071
Askelou Azzeddine <126351103+AzzOu3108@users.noreply.github.com>
2025-01-06 22:46:14 +0100

    projectController

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,3 +1,57 @@
 const projet = require('../models/project')
 const bcrypt = require('bcryptjs')
 
+const getAllProjects = async (req, res) =>{
+    try {
+        const project = await projet.findAll()
+        if(!project.length){
+            res.status(404).json({ message: "Il n'y a pas des projets"})
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Erreur du serveur' });
+    }
+};
+
+
+const getProjectByName = async (req, res) =>{
+    const {nom_de_projet} = req.body
+    if(!nom_de_projet){
+        return res.status(400).json({ message: "Entrez le nom complet de projet" });
+    }
+
+    try {
+        const project = await projet.findOne({ where: { nom_de_projet } });
+        if (!project) {
+            return res.status(404).json({ message: "Il n'y a pas de projet" });
+        }
+        res.json({ project });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Erreur du serveur' }); 
+    }
+};
+
+
+const createProject = async (req, res) =>{
+    const {} = req.body
+};
+
+
+const updateProject = async (req, res) =>{
+
+};
+
+
+const deleteProject = async (req, res) =>{
+
+};
+
+
+module.exports = {
+    getAllProjects,
+    getProjectByName,
+    createProject,
+    updateProject,
+    deleteProject
+}
