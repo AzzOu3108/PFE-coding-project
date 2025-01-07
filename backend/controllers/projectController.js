46d1e54ee95e08b27bc48e817e03362d84a47b45
Askelou Azzeddine <126351103+AzzOu3108@users.noreply.github.com>
2025-01-07 22:57:33 +0100

    radom

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -5,8 +5,9 @@ const getAllProjects = async (req, res) =>{
     try {
         const project = await projet.findAll()
         if(!project.length){
-            res.status(404).json({ message: "Il n'y a pas des projets"})
+            res.status(404).json({ message: "Aucun projet trouvé"})
         }
+        res.json(project)
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: 'Erreur du serveur' });
@@ -15,7 +16,7 @@ const getAllProjects = async (req, res) =>{
 
 
 const getProjectByName = async (req, res) =>{
-    const {nom_de_projet} = req.body
+    const {nom_de_projet} = req.query
     if(!nom_de_projet){
         return res.status(400).json({ message: "Entrez le nom complet de projet" });
     }
@@ -25,7 +26,7 @@ const getProjectByName = async (req, res) =>{
         if (!project) {
             return res.status(404).json({ message: "Il n'y a pas de projet" });
         }
-        res.json({ project });
+        res.json(project)
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: 'Erreur du serveur' }); 
@@ -34,7 +35,14 @@ const getProjectByName = async (req, res) =>{
 
 
 const createProject = async (req, res) =>{
-    const {} = req.body
+    const {
+        utilisateur_id, nom_complet, numero_telephone_utilisateur, function_de_projet,
+        departement, nom_programme, sponsor_de_programme, manager_de_programme,
+        nom_de_projet, countrole_des_couts, description, objective, date_de_debut,
+        date_de_fin, buget_global
+    } = req.body
+
+    
 };
 
 
