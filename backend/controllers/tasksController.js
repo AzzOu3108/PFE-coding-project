51a898bb3703461fc502db7f7ea896add1978a61
Askelou Azzeddine <126351103+AzzOu3108@users.noreply.github.com>
2025-01-12 22:25:47 +0100

    getAllTasks controller

--- backend/controllers/tasksController.js
+++ backend/controllers/tasksController.js
@@ -3,7 +3,20 @@ const projet = require('../models/project')
 const utilisateur = require('../models/users')
 
 const getAllTasks = async (req, res) =>{
-
+    try {
+        const tasks = await tache.findAll({
+            include: [
+                { model: projet, attributes: ['npm_de_projet'] },
+                { model: utilisateur, attributes: ['nom_complet'], as: 'equipe' },
+            ],
+        });
+        if(!tasks.length){
+            return res.status(404).json({ message: "Aucun tache disponible." });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur du serveur." });
+    }
 };
 
 const createTask = async (req, res) =>{
